bdb5308efbb4c1d7674bfd5023b550414fc406a3
Adhinarayanakunchala <adhinarayanak007@gmail.com>
2023-09-30 11:52:59 +0530

    PowerPtx

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+function App() {
+  return (
+    <div className='App'>
+       <iframe src="https://onedrive.live.com/embed?resid=16C39CBE98807636%21107&authkey=!ANemQlqrW6rHSDA&em=2" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+    </div>
+  );
+}
+
+export default App;
